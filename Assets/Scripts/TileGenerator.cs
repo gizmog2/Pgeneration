212e6cc63b76e40dbb8cca7daee28f9e27f2f292
Сергей Буркун <GizmoG2@live.ru>
2022-11-13 21:35:30 +0200

    Создан визуилизатор шума.

--- Assets/Scripts/TileGenerator.cs
+++ Assets/Scripts/TileGenerator.cs
@@ -8,13 +8,26 @@ public class TileGenerator : MonoBehaviour
     public int noiseSampleSize;
     public float scale;
 
+    private MeshRenderer tileMeshRenderer;
+    private MeshFilter tileMeshFilter;
+    private MeshCollider tileMeshColleder;
+
     private void Start()
     {
+        // get the tile components
+        tileMeshColleder = GetComponent<MeshCollider>();
+        tileMeshRenderer = GetComponent<MeshRenderer>();
+        tileMeshFilter = GetComponent<MeshFilter>();
+
         GenerateTile();
     }
 
     void GenerateTile()
     {
         float[,] heightMap = NoiseGenerator.GenerateNoiseMap(noiseSampleSize, scale);
+
+        Texture2D heightMapTexture = TextureBuilder.BuildTexture(heightMap);
+
+        tileMeshRenderer.material.mainTexture = heightMapTexture;
     }
 }
